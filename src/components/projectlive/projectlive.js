d290857b71a077de9a0e15b52e39e6a01df2d690
developython14 <khasarou@gmail.com>
2023-05-27 17:17:18 +0100

    install required package

--- src/components/projectlive/projectlive.js
+++ src/components/projectlive/projectlive.js
@@ -1,11 +1,14 @@
-import React from "react";
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card,CardBody,CardTitle,CardSubtitle,Button } from 'reactstrap';
+import { Card,CardBody,CardTitle,CardSubtitle,Button,CardText ,Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
+import React, { useState } from 'react';
 
 const Projectdetails = (props) => {
+    const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
     // You can access the props passed to the component
     // and use them to render dynamic content
     return (
@@ -13,6 +16,7 @@ const Projectdetails = (props) => {
   style={{
     width: '18rem'
   }}
+  className="m-3"
 >
 <Carousel autoPlay={true} showThumbs={false}>
                 <div>
@@ -27,20 +31,40 @@ const Projectdetails = (props) => {
         </Carousel>
   <CardBody>
     <CardTitle tag="h5">
-      Card title
+      Right App
     </CardTitle>
     <CardSubtitle
       className="mb-2 text-muted"
       tag="h6"
     >
-      Card subtitle
+      Mobile App
     </CardSubtitle>
     <CardText>
       Some quick example text to build on the card title and make up the bulk of the card‘s content.
     </CardText>
-    <Button>
-      Button
+    <Button color="danger" onClick={toggle}>
+      Live Video
     </Button>
+    <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Do Something
+          </Button>{' '}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>  
   </CardBody>
 </Card>
         
