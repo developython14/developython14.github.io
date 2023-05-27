84d59104880cae47f69b6efc415d189cb91d4862
developython14 <khasarou@gmail.com>
2023-05-27 16:10:00 +0100

    install required package

--- src/components/projectlive/projectlive.js
+++ src/components/projectlive/projectlive.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card,CardBody,CardTitle,CardSubtitle,Button } from 'reactstrap';
 
 const Projectdetails = (props) => {
     // You can access the props passed to the component
     // and use them to render dynamic content
     return (
-        <div style={{height:'200px' ,width:'200px'}}>
-        <Carousel autoPlay={true} showThumbs={false}>
+        <Card
+  style={{
+    width: '18rem'
+  }}
+>
+<Carousel autoPlay={true} showThumbs={false}>
                 <div>
                     <img src="https://images.pexels.com/photos/3934616/pexels-photo-3934616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
                 </div>
@@ -20,7 +25,26 @@ const Projectdetails = (props) => {
                     <img src="https://images.pexels.com/photos/3934616/pexels-photo-3934616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
                 </div>
         </Carousel>
-        </div>
+  <CardBody>
+    <CardTitle tag="h5">
+      Card title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Card subtitle
+    </CardSubtitle>
+    <CardText>
+      Some quick example text to build on the card title and make up the bulk of the card‘s content.
+    </CardText>
+    <Button>
+      Button
+    </Button>
+  </CardBody>
+</Card>
+        
+    
     );
   };
   
